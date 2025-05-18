c4e23db2772e347aeb04d9742a06019230435c74
luspi1 <igbabin566@gmail.com>
2025-05-18 11:56:48 +0300

    изучение декораторов

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MovieModule } from './movie/movie.module';
-import { TaskModule } from './task/task.module';
 
 @Module({
 	controllers: [AppController],
 	providers: [AppService],
-	imports: [MovieModule, TaskModule],
+	imports: [MovieModule],
 })
 export class AppModule {}
--- src/movie/movie.controller.ts
+++ src/movie/movie.controller.ts
@@ -1,7 +1,39 @@
-import { Controller } from '@nestjs/common';
-import { MovieService } from './movie.service';
+import { Body, Controller, Get, Post, Query, Headers, Req, Res, Param } from '@nestjs/common';
+import type { Response, Request } from 'express';
 
-@Controller('movie')
+@Controller({
+	path: 'movie',
+})
 export class MovieController {
-	constructor(private readonly movieService: MovieService) {}
+	@Get()
+	findAll(@Query() genre: any) {
+		return `Жанр ${JSON.stringify(genre)}`;
+	}
+
+	@Get(':id')
+	findById(@Param() param: string) {
+		return param;
+	}
+
+	@Post()
+	create(@Body() body: { title: string; genre: string }) {
+		return `Фильм в жанре ${body.genre ?? 'нет жанра'} и с названием "${body.title}" добавлен`;
+	}
+
+	@Get('headers')
+	getHeaders(@Headers() header: Headers) {
+		return header;
+	}
+
+	@Get('details')
+	getRequestDetails(@Req() request: Request) {
+		return {
+			method: request.method,
+		};
+	}
+
+	@Get('response-details')
+	getResponseDetails(@Res() res: Response) {
+		res.status(666).json({ message: 'dddd' });
+	}
 }
