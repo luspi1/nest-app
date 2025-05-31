d09cd19eafeb89e9c6669490b7978e1039ed904f
luspi1 <igbabin566@gmail.com>
2025-05-31 14:50:17 +0300

    TypeORM

--- src/movie/entities/movie.entity.ts
+++ src/movie/entities/movie.entity.ts
@@ -3,14 +3,27 @@ import {
 	CreateDateColumn,
 	UpdateDateColumn,
 	Entity,
-	PrimaryColumn,
-	Generated,
+	OneToMany,
+	PrimaryGeneratedColumn,
+	ManyToMany,
+	JoinTable,
+	OneToOne,
+	JoinColumn,
 } from 'typeorm';
+import { ReviewEntity } from '../../review/entities/review.entity';
+import { ActorEntity } from '../../actor/entities/actor.entity';
+import { MoviePosterEntity } from './poster.entity';
+
+export enum Genre {
+	ACTION = 'action',
+	COMEDY = 'comedy',
+	DRAMA = 'drama',
+	HORROR = 'horror',
+}
 
 @Entity({ name: 'movies' })
 export class MovieEntity {
-	@PrimaryColumn()
-	@Generated('uuid')
+	@PrimaryGeneratedColumn('uuid')
 	id: string;
 
 	@Column()
@@ -23,17 +36,54 @@ export class MovieEntity {
 	description: string;
 
 	@Column({
+		name: 'release_year',
 		type: 'int',
 		unsigned: true,
 	})
 	releaseYear: number;
 
-	@Column({ default: false })
-	isPublic: boolean;
+	@Column({
+		type: 'decimal',
+		precision: 3,
+		scale: 1,
+		default: 0.0,
+	})
+	rating: number;
 
-	@CreateDateColumn()
+	@Column({ name: 'is_available', type: 'boolean', default: false })
+	isAvailable: boolean;
+
+	@Column({
+		type: 'enum',
+		enum: Genre,
+		default: Genre.DRAMA,
+	})
+	genre: Genre;
+
+	@Column({ name: 'poster_id', type: 'uuid', nullable: true })
+	posterId: string;
+
+	@OneToOne(() => MoviePosterEntity, (poster) => poster.movie, {
+		onDelete: 'CASCADE',
+		nullable: true,
+	})
+	@JoinColumn({ name: 'poster_id' })
+	poster: MoviePosterEntity | null;
+
+	@OneToMany(() => ReviewEntity, (review) => review.movie)
+	reviews: ReviewEntity[];
+
+	@ManyToMany(() => ActorEntity, (actor) => actor.movies)
+	@JoinTable({
+		name: 'movie_actors',
+		joinColumn: { name: 'movie_id', referencedColumnName: 'id' },
+		inverseJoinColumn: { name: 'actor_id', referencedColumnName: 'id' },
+	})
+	actors: ActorEntity[];
+
+	@CreateDateColumn({ name: 'created_at' })
 	createdAt: Date;
 
-	@UpdateDateColumn()
+	@UpdateDateColumn({ name: 'updated_at' })
 	updatedAt: Date;
 }
--- src/movie/movie.service.ts
+++ src/movie/movie.service.ts
@@ -1,13 +1,18 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
+import { In, Repository } from 'typeorm';
 import { MovieEntity } from './entities/movie.entity';
 import { MovieDto } from './dto/movie.dto';
+import { ActorEntity } from '../actor/entities/actor.entity';
+import { MoviePosterEntity } from './entities/poster.entity';
 
 @Injectable()
 export class MovieService {
 	constructor(
 		@InjectRepository(MovieEntity) private readonly movieRepository: Repository<MovieEntity>,
+		@InjectRepository(MoviePosterEntity)
+		private readonly posterRepository: Repository<MoviePosterEntity>,
+		@InjectRepository(ActorEntity) private readonly actorRepository: Repository<ActorEntity>,
 	) {}
 
 	async findAll(): Promise<MovieEntity[]> {
@@ -19,7 +24,29 @@ export class MovieService {
 	}
 
 	async create(dto: MovieDto): Promise<MovieEntity> {
-		const movie = this.movieRepository.create(dto);
+		const { actorIds, title, releaseYear, imageUrl } = dto;
+
+		const actors = await this.actorRepository.find({
+			where: {
+				id: In(actorIds),
+			},
+		});
+
+		if (!actors?.length) throw new NotFoundException('Один или несколько актеров не найдены');
+
+		let poster: MoviePosterEntity | null = null;
+
+		if (imageUrl) {
+			poster = this.posterRepository.create({ url: imageUrl });
+			await this.posterRepository.save(poster);
+		}
+
+		const movie = this.movieRepository.create({
+			title,
+			releaseYear,
+			poster,
+			actors,
+		});
 		return await this.movieRepository.save(movie);
 	}
 
@@ -28,6 +55,7 @@ export class MovieService {
 			where: {
 				id,
 			},
+			relations: ['actors'],
 		});
 
 		if (!searchedMovie) throw new NotFoundException('Фильм не найден');
